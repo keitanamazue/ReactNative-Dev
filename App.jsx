172df1e5c397b7f72a8246d088b7096711b35af7
keita namazue <keitanamazue@gmail.com>
2021-12-05 16:44:43 +0900

    ⚡️ reactnative tutorial

--- App.jsx
+++ App.jsx
@@ -1,11 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import Hello from "./src/components/Hello";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>im keita up App.js keitaaaaa app!</Text>
+      <Hello
+        bang
+        style={{
+          fontSize: 10,
+        }}
+      >
+        kuriyama
+      </Hello>
+      <Hello bang>keita</Hello>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +26,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: "#eee",
   },
 });

9961725db293e99540731d02736273be865dc0ae
keita namazue <keitanamazue@gmail.com>
2021-12-07 02:03:38 +0900

    ⚡️ add memoCreate, Edit

--- App.jsx
+++ App.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import MemoCreateScreen from "./src/screens/MemoCreateScreen";
 import MemoDetailScreen from "./src/screens/MemoDetailScreen";
+import MemoEditScren from "./src/screens/MemoEditScren";
 import MemoListScreen from "./src/screens/MemoListScreen";
 
 export default function App() {
-  return <MemoListScreen />;
+  return <MemoCreateScreen />;
 }

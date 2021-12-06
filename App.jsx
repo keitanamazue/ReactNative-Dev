d6c6fafcff8ed123490247f9b4c16c5cc9222b7d
keita namazue <keitanamazue@gmail.com>
2021-12-07 02:16:11 +0900

    ⚡️ fix the KeyboardAvoidingView

--- App.jsx
+++ App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import MemoCreateScreen from "./src/screens/MemoCreateScreen";
 import MemoDetailScreen from "./src/screens/MemoDetailScreen";
-import MemoEditScren from "./src/screens/MemoEditScren";
+import MemoEditScreen from "./src/screens/MemoEditScreen";
 import MemoListScreen from "./src/screens/MemoListScreen";
 
 export default function App() {
-  return <MemoCreateScreen />;
+  return <MemoEditScreen />;
 }

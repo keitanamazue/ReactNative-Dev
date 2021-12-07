e57ba159625521ad39af4bad1dd6fb191da1c1a6
keita namazue <keitanamazue@gmail.com>
2021-12-07 17:07:46 +0900

    ✅ add signup, login page

--- App.jsx
+++ App.jsx
@@ -3,7 +3,9 @@ import MemoCreateScreen from "./src/screens/MemoCreateScreen";
 import MemoDetailScreen from "./src/screens/MemoDetailScreen";
 import MemoEditScreen from "./src/screens/MemoEditScreen";
 import MemoListScreen from "./src/screens/MemoListScreen";
+import LoginScreen from "./src/screens/LoginScreen";
+import SignUpScreen from "./src/screens/SignUpScreen";
 
 export default function App() {
-  return <MemoEditScreen />;
+  return <SignUpScreen />;
 }
